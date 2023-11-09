c76d71ff637f4fd7f7dd838616262267c45f8452
DiscoverProgramming <150089635+DiscoverProgramming@users.noreply.github.com>
2023-11-09 09:22:18 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from lib import lib
 
 delay = float(input("Delay between screenshots(Seconds): "))
-start_index = int(input("Starting index for image names: "))
 interval = float(input("Interval between frames shown on video(Seconds): "))
 
+start_index = 0
 
 capture = lib(delay=delay, start_index=start_index, dir="imgs/", interval=interval)
 
